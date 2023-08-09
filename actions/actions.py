28f39dcc3b5e3485dd6f043457cfac4a5de745e7
Duong <nguyenduong7412@gmail.com>
2023-08-09 12:15:44 +0700

    cap nhat kiem tra ket qua cua tung mien

--- actions/actions.py
+++ actions/actions.py
@@ -7,7 +7,6 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
-
 import requests
 import json
 import feedparser
@@ -39,6 +38,65 @@ class action_get_lottery(Action):
             return_msg += "\n" + first_node_nam[0]['title'] + "\n" + first_node_nam[0]['description']
 
             return_msg += "\n" + first_node_trung[0]['title'] + "\n" + first_node_trung[0]['description']
+
+            dispatcher.utter_message(return_msg)
+            return []
+
+class action_get_lottery_bac(Action):
+   def name(self):
+          return 'action_get_lottery_bac'
+
+   def run(self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+            url_bac = 'https://xskt.com.vn/rss-feed/mien-bac-xsmb.rss'
+
+
+            feed_bac = feedparser.parse(url_bac)
+            first_node = feed_bac['entries']
+
+            return_msg = first_node[0]['title'] + "\n" + first_node[0]['description']
+
+            dispatcher.utter_message(return_msg)
+            return []
+
+class action_get_lottery_trung(Action):
+   def name(self):
+          return 'action_get_lottery_trung'
+
+   def run(self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+            url_trung= 'https://xskt.com.vn/rss-feed/mien-trung-xsmt.rss'
+
+
+            feed_trung = feedparser.parse(url_trung)
+            first_node = feed_trung['entries']
+
+            return_msg = first_node[0]['title'] + "\n" + first_node[0]['description']
+
+            dispatcher.utter_message(return_msg)
+            return []
+
+class action_get_lottery_nam(Action):
+   def name(self):
+          return 'action_get_lottery_nam'
+
+   def run(self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+            url_nam = 'https://xskt.com.vn/rss-feed/mien-nam-xsmn.rss'
+
+            feed_nam = feedparser.parse(url_nam)
+            first_node = feed_nam['entries']
+
+            return_msg = first_node[0]['title'] + "\n" + first_node[0]['description']
 
             dispatcher.utter_message(return_msg)
             return []
